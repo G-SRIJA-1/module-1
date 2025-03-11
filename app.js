4f7cd2632beb78a2831d442a8652f73c7efccd9d
G Srija <gsri1498@gmail.com>
2025-03-11 14:44:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+  
+    angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.lunchItems = '';
+      $scope.message = '';
+  
+      $scope.checkLunchItems = function () {
+        var items = $scope.lunchItems.trim();
+        if (!items) {
+          $scope.message = 'Please enter data first';
+          return;
+        }
+  
+        var itemArray = items.split(',');
+  
+        itemArray = itemArray.filter(function (item) {
+          return item.trim() !== '';
+        });
+  
+        if (itemArray.length <= 3) {
+          $scope.message = 'Enjoy!';
+        } else {
+          $scope.message = 'Too much!';
+        }
+      };
+    }
+  })();
